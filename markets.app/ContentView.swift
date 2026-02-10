4ea232a65b5c565f79526d9b89dc951fcea88f55
Azoz9ll <khaldzwz047@gmail.com>
2026-02-10 17:01:29 +0300

    تعديل

--- markets.app/ContentView.swift
+++ markets.app/ContentView.swift
@@ -3,8 +3,8 @@
 //  markets.app
 //
 //  Created by abdulaziz on 10/02/2026.
-//هذا الملف للتعديل على واجهة المستخدم 
-
+//هذا الملف للتعديل على واجهة المستخدم
+// اسم المكتبة
 import SwiftUI
 
 struct ContentView: View {
